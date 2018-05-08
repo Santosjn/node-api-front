2c6bd4c85e40b74b661d01d4742642d14cba6a38
santosjn <jeansjcsp@gmail.com>
2018-05-08 12:15:19 -0300

    Configuring for better styling. Adding an app bar.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Wrapper from '../Wrapper/Wrapper';
 
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import classes from './Layout.css';
+
 
 const layout = (props) => {
-    return(
+   
+    return (
         <Wrapper>
-            <div>Toolbar</div>
-            <main>
+            <AppBar position="static" color="primary">
+                <Toolbar>
+                    <Typography variant="title" color="inherit">
+                        Climatempo
+                    </Typography>   
+                </Toolbar>
+            </AppBar>
+            
+            <main className={classes.Content}>
                 {props.children}
             </main>
         </Wrapper>
